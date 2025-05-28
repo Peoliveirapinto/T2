da644db04f2a679989067ca135a88c5e0bf4e051
Pedro Oliveira <peoliveirapinto@gmail.com>
2025-05-28 13:55:37 -0300

    add fila fix documento

--- Documento.java
+++ Documento.java
@@ -4,6 +4,8 @@ public class Documento {
     String nomeArquivo;
     String nomeUsuario;
     long horarioSolicitacao;
+    long horarioImpressao;
+    long horarioReimpressao;
     public String getNomeArquivo() {
         return nomeArquivo;
     }
@@ -21,5 +23,26 @@ public class Documento {
     }
     public void setHorarioSolicitacao() {
         this.horarioSolicitacao = new Date().getTime();
+    }
+    public long getHorarioImpressao() {
+        return horarioImpressao;
+    }
+    public void setHorarioImpressao() {
+        this.horarioImpressao = new Date().getTime();
+    }
+    public long getHorarioReimpressao() {
+        return horarioReimpressao;
+    }
+    public void setHorarioReimpressao() {
+        this.horarioReimpressao = new Date().getTime();
+    }
+    @Override
+    public String toString() {
+        return
+                nomeArquivo + '\n' +
+                "Nome do usuário = " + nomeUsuario + '\n' +
+                "Horário solicitado = " + horarioSolicitacao + '\n' +
+                "Horário impressão = " + horarioImpressao + '\n' +
+                "Horário reimpressao = " + horarioReimpressao + '\n';
     }
 }
--- Fila.java
+++ Fila.java
@@ -1,3 +1,73 @@
 public class Fila {
-    
+    Documento[] dados;
+    int primeiro, ultimo, ocupacao;
+    public Fila (int capacidade) {
+        dados = new Documento[capacidade];
+        //por clareza
+        primeiro = 0;
+        ultimo = 0;
+        ocupacao = 0;
+    }
+    public Fila () {
+        this(10);
+    }
+    public boolean filaVazia() {
+        return ocupacao == 0;
+    }
+    public boolean filaCheia() {
+        return ocupacao == dados.length;
+    }
+    private int proxima (int pos) {
+        return (pos + 1) % dados.length;
+    }
+    public void enfileira (Documento e) {
+        if (filaCheia()) throw new RuntimeException("falha no enfileiramento");
+        dados[ultimo] = e;
+        ultimo = proxima(ultimo);
+        ocupacao++;
+    }
+    public Documento desenfileira () {
+        if (filaVazia()) throw new RuntimeException("falha no desenfileiramento");
+        Documento temp = dados[primeiro];
+        primeiro = proxima(primeiro);
+        ocupacao--;
+        return temp;
+    }
+    @Override
+    public String toString () {
+        if (filaVazia()) return "fila vazia";
+        String s = "";
+        for (int i=primeiro, cont=0; cont < ocupacao; cont++) {
+            s += dados[i] + " ";
+            i = proxima(i);
+        }
+        return s;
+    }
+    public String stringVetor() {
+        String s="";
+        int i;
+        if (filaVazia())
+            for (i=0; i<dados.length; i++)
+                s += "_ ";
+        else if (filaCheia())
+            for(i=0; i<dados.length; i++)
+                s += dados[i] + " ";
+        else if (primeiro < ultimo) {
+            for (i=0; i < primeiro; i++)
+                s += "_ ";
+            for (i=primeiro; i<ultimo; i++)
+                s += dados[i] + " ";
+            for (i=ultimo; i<dados.length; i++)
+                s += "_ ";
+        }
+        else {
+            for (i=0; i < ultimo; i++)
+                s += dados[i] + " ";
+            for (i=ultimo; i<primeiro; i++)
+                s += "_ ";
+            for (i=primeiro; i<dados.length; i++)
+                s += dados[i] + " ";
+        }
+        return s;
+    }
 }
